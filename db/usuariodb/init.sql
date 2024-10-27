8f7f573283a2d50542738f28b48ce10b00f3d703
ntlg72 <166937483+ntlg72@users.noreply.github.com>
2024-10-26 20:27:33 -0500

    Update init.sql

--- db/usuariodb/init.sql
+++ db/usuariodb/init.sql
@@ -12,3 +12,9 @@ CREATE TABLE usuarios (
     direccion VARCHAR(100),
     documento_de_identidad VARCHAR(20)
 );
+
+USE usuarios_BD;
+
+
+INSERT INTO usuarios (username, email, nombre, password, customer_city, direccion, documento_de_identidad) VALUES ('admin', 'admin@example.com', 'Hugh Womble', '1234', 'Cartagena', 'Cr. 167 Bis Q # 4-96 Sur, El Tarra, 706881', '615957315');
+INSERT INTO usuarios (username, email, nombre, password, customer_city, direccion, documento_de_identidad) VALUES ('martin.ling7414', 'luis34@example.net', 'Martin Ling', '9_44+MN9jk', 'Bogotá', 'Av. San Francisco # 92Y-6 Sur, Guayabetal, 542375', '6238537797');
